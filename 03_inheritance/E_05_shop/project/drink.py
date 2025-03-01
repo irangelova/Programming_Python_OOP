387b431d5e6d0ed3fdb1fe261632c760b77641c4
irangelova <ivetarangelova@gmail.com>
2025-03-01 18:15:21 +0100

    Solutions to tasks from inheritance lecture

---
+++ 03_inheritance/E_05_shop/project/drink.py
@@ -0,0 +1,9 @@
+from project.product import Product
+
+
+class Drink(Product):
+    #def __int__(self, name: str, quantity: int = 10):
+     #   super().__init__(name, quantity)
+
+    def __init__(self, name: str):
+        super().__init__(name, 10)
---
+++ 03_inheritance/E_05_shop/project/product_repository.py
@@ -0,0 +1,24 @@
+import pickletools
+from typing import List, Optional
+
+from project.product import Product
+
+
+class ProductRepository:
+    def __init__(self):
+        self.products: List[Product] = []
+
+    def add(self, product: Product):
+        self.products.append(product)
+
+    def find(self, product_name: str) -> Optional[Product]:
+        return next((product for product in self.products if product.name == product_name), None)
+
+    def remove(self, product_name: str):
+        product = self.find(product_name)
+        if product:
+            self.products.remove(product)
+
+    def __repr__(self):
+        return "\n".join([f'{product.name}: {product.quantity}' for product in self.products])
+
